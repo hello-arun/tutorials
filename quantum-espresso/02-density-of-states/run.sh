13522c661301ad026ae806d11eae8e810ece59f4
Arun Jangir <jangirarun786@gmail.com>
2022-08-31 17:06:40 +0300

    minor coorection in 02-DOS

--- quantum-espresso/02-density-of-states/run.sh
+++ quantum-espresso/02-density-of-states/run.sh
@@ -9,7 +9,7 @@ mkdir -p ${calcDIR}
 
 # Copy required files
 cd $dataDIR
-cp dos.in  INCAR-nscf.in  INCAR-scf.in  run.sbatch  Si.pz-vbc.UPF  $calcDIR/
+cp dos.in  INCAR-nscf.pw  INCAR-scf.pw  run.sbatch  Si.pz-vbc.UPF  $calcDIR/
 
 cd $calcDIR
 sed -i "s/__jObName/Si-DOS/" ${calcDIR}/run.sbatch
