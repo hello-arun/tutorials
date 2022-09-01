1322da0204ab9ca45b9ba4f164d2237896ed5c63
Arun Jangir <jangirarun786@gmail.com>
2022-09-01 19:57:33 +0300

    Polarization lattice implemented

--- quantum-espresso/polarisation/example-02/data/grep-polarization.sh
+++ quantum-espresso/polarisation/example-02/data/grep-polarization.sh
@@ -3,11 +3,12 @@
 strains=$(seq -0.010 0.005 0.010)
 outFile="polarization.csv"
 touch ${outFile}
-echo "strain, polarization" > ${outFile}
-echo "#_unitless, coulomb/meter^2" >> ${outFile}
+echo "strain, polarization, quanta" > ${outFile}
+echo "#_unitless, coulomb/meter^2,  coulomb/meter^2" >> ${outFile}
 for strain in $strains; do 
     pol=$(grep "P = " ./ep_x${strain}/OUTCAR-nscf.pw | tail -1 | awk '{print $3}')
-    echo "${strain}, ${pol}" >> $outFile
+    quanta=$(grep "P = " ./ep_x${strain}/OUTCAR-nscf.pw | tail -1 | awk '{print $5}')
+    echo "${strain}, ${pol}, ${quanta/)/}" >> $outFile
 done
 
 column -t $outFile > ./temp
--- quantum-espresso/polarisation/example-02/run.sh
+++ quantum-espresso/polarisation/example-02/run.sh
@@ -11,7 +11,7 @@ mkdir -p ${calcDIR}
 # Copy required files
 cd $dataDIR
 pseudos="B_pbe_v1.01.uspp.F.UPF N.oncvpsp.upf"
-cp INCAR-*.pw grep-polarization.sh plot-polarization.py  ${pseudos} run.sbatch $calcDIR/
+cp INCAR-*.pw grep-polarization.sh plot-polarization.py plot-polarization-lattice.py  ${pseudos} run.sbatch $calcDIR/
 
 cd $calcDIR
 sed -i "s/__jObName/Polar-hBN/" ${calcDIR}/run.sbatch
