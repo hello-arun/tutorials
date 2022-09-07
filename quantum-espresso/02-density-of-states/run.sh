cb6fa7620d5617d91615dac20cbbca9199992eef
Arun Jangir <jangirarun786@gmail.com>
2022-09-08 00:22:00 +0300

    File refactoring

--- quantum-espresso/02-density-of-states/run.sh
+++ quantum-espresso/02-density-of-states/run.sh
@@ -9,7 +9,7 @@ mkdir -p ${calcDIR}
 
 # Copy required files
 cd $dataDIR
-cp dos.in plot-dos.py INCAR-nscf.pw  INCAR-scf.pw  run.sbatch  Si.pz-vbc.UPF  $calcDIR/
+cp plot-dos.py INCAR*  run.sbatch  *.UPF  $calcDIR/
 
 cd $calcDIR
 sed -i "s/__jObName/Si-DOS/" ${calcDIR}/run.sbatch
