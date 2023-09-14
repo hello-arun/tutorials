97e1790810779a59a7a08131751e5978655ae9da
Arun <jangirarun786@gmail.com>
2023-09-14 15:07:23 +0300

    Update

--- 05-atomisticToolsPython/ovito/02-makeSuperCell.py
+++ 05-atomisticToolsPython/ovito/02-makeSuperCell.py
@@ -3,7 +3,7 @@ from math import ceil
 from ovito.io import import_file,export_file
 from ovito.pipeline import Pipeline
 from ovito.data import DataCollection,Particles
-from ovito.modifiers import ExpressionSelectionModifier,ReplicateModifier,ExpandSelectionModifier,DeleteSelectedModifier,InvertSelectionModifier
+from ovito.modifiers import AffineTransformationModifier,ExpressionSelectionModifier,ReplicateModifier,ExpandSelectionModifier,DeleteSelectedModifier,InvertSelectionModifier
 import numpy as np
 import os
 logging.basicConfig(
@@ -19,32 +19,67 @@ logging.basicConfig(
 radiusODPA = 100 # Radius of ODPA region  
 radiusAu = 45 
 gapAuODPA = 10 
-refFile="./output-topo.lmp"
+zUnitAu = 5
+relShiftODPA = 5
+
+L_MAX=2*(radiusAu+radiusODPA+gapAuODPA)
+refODPAFile="./output-topo.lmp"
+refAuFile="./input-Au-unitCell.lmp"
 outDIR="./supCell"
 os.makedirs(outDIR,exist_ok=True)
 
-pipeline:Pipeline = import_file(refFile,sort_particles=True)
-data:DataCollection=pipeline.compute()
-Lx,Ly,Lz=data.cell_[0,0],data.cell_[1,1],data.cell_[2,2]
-sx=ceil(2*radiusODPA/Lx)
-sy=ceil(2*radiusODPA/Ly)
-replicate=ReplicateModifier(num_x=sx,num_y=sy,num_z=1)
-pipeline.modifiers.append(replicate)
-data=pipeline.compute()
-Lx,Ly,Lz=data.cell[0,0],data.cell[1,1],data.cell[2,2]
-cx,cy=Lx*0.5+data.cell[0,3],Ly*0.5+data.cell[1,3]
+# First Prepare ODPA 
+pipe_ODPA:Pipeline = import_file(refODPAFile,sort_particles=True)
+data_ODPA:DataCollection=pipe_ODPA.compute()
+sx=ceil(2*radiusODPA/data_ODPA.cell[0,0])
+sy=ceil(2*radiusODPA/data_ODPA.cell[1,1])
+data_ODPA.apply(ReplicateModifier(num_x=sx,num_y=sy,num_z=1))
+cx,cy=data_ODPA.cell[0,0]*0.5+data_ODPA.cell[0,3],data_ODPA.cell[1,1]*0.5+data_ODPA.cell[1,3] # center of cell
 expressionSelection = ExpressionSelectionModifier(
-    expression=f'(Position.X-{cx})*(Position.X-{cx})+(Position.Y-{cy})*(Position.Y-{cy})<{radiusODPA*radiusODPA}'
+    expression=f'(Position.X-{cx})*(Position.X-{cx})+(Position.Y-{cy})*(Position.Y-{cy})>{radiusODPA*radiusODPA}'
 )
 expandSelection = ExpandSelectionModifier(
     iterations=20, # depth of recursion
     mode=ExpandSelectionModifier.ExpansionMode.Bonded
 )
-invertSelection=InvertSelectionModifier()
 deleteSelction=DeleteSelectedModifier()
-pipeline.modifiers.append(expressionSelection)
-pipeline.modifiers.append(expandSelection)
-pipeline.modifiers.append(invertSelection)
-pipeline.modifiers.append(deleteSelction)
-data=pipeline.compute()
-export_file(data, f"{outDIR}/ODPA-R{radiusODPA:03d}.lmp", "lammps/data", atom_style="full")
+data_ODPA.apply(expressionSelection)
+data_ODPA.apply(expandSelection)
+data_ODPA.apply(deleteSelction)
+export_file(data_ODPA, f"{outDIR}/temp-1.lmp", "lammps/data", atom_style="full")
+
+# Now Prepare AU
+pipe_Au=import_file(refAuFile,sort_particles=True)
+data_Au=pipe_Au.compute()
+sx_Au,sy_Au,sz_Au=ceil(L_MAX/data_Au.cell[0,0]),ceil(L_MAX/data_Au.cell[1,1]),zUnitAu
+data_Au.apply(ReplicateModifier(num_x=sx_Au,num_y=sy_Au,num_z=sz_Au))
+cx,cy=data_Au.cell[0,0]*0.5+data_Au.cell[0,3],data_Au.cell[1,1]*0.5+data_Au.cell[1,3] # center of cell
+data_Au.apply(ExpressionSelectionModifier(
+    expression=f'(Position.X-{cx})*(Position.X-{cx})+(Position.Y-{cy})*(Position.Y-{cy})<{(radiusODPA+gapAuODPA)**2}'
+))
+data_Au.apply(DeleteSelectedModifier())
+export_file(data_Au, f"{outDIR}/temp-2.lmp", "lammps/data", atom_style="full")
+
+# Now Adjust set the unitCell origin to 0,0 for both Au and ODPA
+data_Au.cell_[:,3] = 0
+data_Au.cell_[2,2]=100
+data_ODPA.cell_=data_Au.cell
+cx,cy=data_Au.cell[0,0]*0.5+data_Au.cell[0,3],data_Au.cell[1,1]*0.5+data_Au.cell[1,3] # center of cell
+
+# Shift Atoms to match perfectly
+data_Au.particles_.positions_ -= [
+    np.min(data_Au.particles_.positions[:,0]),
+    np.min(data_Au.particles_.positions[:,1]),
+    np.min(data_Au.particles_.positions[:,2])
+]
+data_ODPA.particles_.positions_ -= [
+    np.average(data_ODPA.particles_.positions[:,0])-cx,
+    np.average(data_ODPA.particles_.positions[:,1])-cy,
+    np.min(data_ODPA.particles_.positions[:,2])-np.max(data_Au.particles_.positions[:,2]+relShiftODPA)
+]
+
+export_file(data_ODPA, f"{outDIR}/temp-3.lmp", "lammps/data", atom_style="full")
+export_file(data_Au, f"{outDIR}/temp-4.lmp", "lammps/data", atom_style="full")
+
+export_file(data_ODPA, f"{outDIR}/ODPA-RO_{radiusODPA:03d}-RAu_{radiusAu:03d}-Gap_{gapAuODPA:02d}.lmp", "lammps/data", atom_style="full")
+export_file(data_Au, f"{outDIR}/Au-RO_{radiusODPA:03d}-RAu_{radiusAu:03d}-Gap_{gapAuODPA:02d}.lmp", "lammps/data", atom_style="full")
--- 05-atomisticToolsPython/ovito/03-merge.py
+++ 05-atomisticToolsPython/ovito/03-merge.py
@@ -1,63 +1,16 @@
-import logging
 from math import ceil
 from ovito.io import import_file,export_file
-from ovito.pipeline import Pipeline
-from ovito.data import DataCollection,Particles
-from ovito.modifiers import CombineDatasetsModifier,AffineTransformationModifier,ExpressionSelectionModifier,ReplicateModifier,ExpandSelectionModifier,DeleteSelectedModifier,InvertSelectionModifier
-import numpy as np
-import os
-logging.basicConfig(
-    level=logging.INFO,          # Set the logging level to DEBUG (or other level)
-    format='%(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
-    handlers=[
-        logging.FileHandler('03-mergeAuODPA.log',mode="w"),  # Log to a file
-        logging.StreamHandler()            # Log to console (stream)
-    ]
-)
+from ovito.modifiers import CombineDatasetsModifier
 
-radiusODPA = 100 # Radius of ODPA region  
-radiusAu = 45 
+rODPA = 100 # Radius of ODPA region  
+rAu = 45 
 gapAuODPA = 10 
-shiftzODPA=5
 outDIR="./supCell"
-refODPAFile=f"{outDIR}/ODPA-R{radiusODPA:03d}.lmp"
-refAuFile="./input-Au-unitCell.lmp"
-os.makedirs(outDIR,exist_ok=True)
-pipe_ODPA:Pipeline = import_file(refODPAFile,sort_particles=True)
-pipe_Au=import_file(refAuFile,sort_particles=True)
-data_ODPA:DataCollection=pipe_ODPA.compute()
-data_Au=pipe_Au.compute()
-lf,lx_Au,ly_Au=2*(radiusAu+radiusODPA+gapAuODPA),data_Au.cell[0,0],data_Au.cell[1,1]
-sx_Au,sy_Au,sz_Au=ceil(lf/lx_Au),ceil(lf/ly_Au),5
-replicate=ReplicateModifier(num_x=sx_Au,num_y=sy_Au,num_z=sz_Au)
-data_Au.apply(modifier=replicate) # Change the data inplace
-data_Au.cell_[:,3]  =  0   # setOriginToZero
-data_Au.cell_[2,2]  = 100  # change the c vector to 100 Angstrom
-data_ODPA.cell_[:,3] = 0   # We set the origin to 0 so that we not have to worry about 
-Lx,Ly=data_Au.cell[0,0],data_Au.cell[1,1]
-cx,cy=Lx*0.5,Ly*0.5
-data_ODPA.cell_=data_Au.cell
-data_Au.particles_.positions_ -= [np.min(data_Au.particles_.positions[:,0]),np.min(data_Au.particles_.positions[:,1]),np.min(data_Au.particles_.positions[:,2])]
-data_ODPA.particles_.positions_ -= [
-    np.average(data_ODPA.particles_.positions[:,0])-cx,
-    np.average(data_ODPA.particles_.positions[:,1])-cy,
-    np.min(data_ODPA.particles_.positions[:,2])-shiftzODPA]
 
-expressionSelection = ExpressionSelectionModifier(
-    expression=f'(Position.X-{cx})*(Position.X-{cx})+(Position.Y-{cy})*(Position.Y-{cy})<{(radiusODPA+gapAuODPA)**2}'
-)
-deleteSelection=DeleteSelectedModifier()
-data_Au.apply(modifier=expressionSelection)
-data_Au.apply(modifier=deleteSelection)
-
-export_file(data_Au,"./temp1.lmp", "lammps/data", atom_style="full")
-export_file(data_ODPA,"./temp2.lmp", "lammps/data", atom_style="full")
-
-pipeline = import_file('temp2.lmp')
+pipeline = import_file(f'{outDIR}/ODPA-RO_{rODPA:03d}-RAu_{rAu:03d}-Gap_{gapAuODPA:02d}.lmp')
 # Insert the particles from a second file into the dataset. 
 modifier = CombineDatasetsModifier()
-modifier.source.load("./temp1.lmp")
+modifier.source.load(f'{outDIR}/Au-RO_{rODPA:03d}-RAu_{rAu:03d}-Gap_{gapAuODPA:02d}.lmp')
 pipeline.modifiers.append(modifier)
-export_file(pipeline,f"{outDIR}/merged.lmp", "lammps/data", atom_style="full")
-export_file(pipeline,f"{outDIR}/temp3.lmp", "lammps/data", atom_style="full")
+export_file(pipeline,f'{outDIR}/merged-RO_{rODPA:03d}-RAu_{rAu:03d}-Gap_{gapAuODPA:02d}.lmp', "lammps/data", atom_style="full")
+export_file(pipeline,f"{outDIR}/temp-5.lmp", "lammps/data", atom_style="full")
