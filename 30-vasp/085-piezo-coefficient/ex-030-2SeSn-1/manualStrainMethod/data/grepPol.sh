78b0d556a2d656551de21f44cf5bd9421277b320
Arun Jangir <jangirarun786@gmail.com>
2025-01-13 16:22:31 +0300

    Automatic Piezoelectric Calculation Method Added

---
+++ 30-vasp/085-piezo-coefficient/ex-030-2SeSn-1/manualStrainMethod/data/grepPol.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+strains=$(seq -0.008 0.004 0.008)
+polAxis="1" # polarization along x,or y axis.. 1:x, 2:y
+strainAxisLabel="Y" # Polarization along X or Y Axis
+
+polAxisMarker=$((5+$polAxis))
+outFile="polarization.csv"
+
+echo "strainAxisLabel, polAxis, epsilon, pIon, pEle, vol, a, b, c, shift" > ${outFile}
+echo "#Char, Integer, Float, e*Angst, e*Angst, Ang^3, Ang, Ang, Ang, Integer" >> ${outFile}
+
+for strain in $strains; do
+    OUTCAR="./ep-${strainAxisLabel}-${strain}/pol-LBERRY/OUTCAR"
+    pEle=$(grep "p\[elc\]" $OUTCAR | awk "{print \$$polAxisMarker}")
+    pIon=$(grep "p\[ion\]" $OUTCAR | awk "{print \$$(($polAxisMarker-1))}")
+    vol=$(grep "volume of cell" $OUTCAR | tail -1 | awk '{print $5}')
+    cellVectors=$(grep "length of vectors" $OUTCAR -A1 | tail -1 | awk '{print $1", "$2", "$3}')
+    echo "$strainAxisLabel, $polAxis, $strain, $pIon, $pEle, $vol", $cellVectors, 0 >> ${outFile}
+done
+
+column -t ${outFile} > temp
+mv temp $outFile
---
+++ 30-vasp/085-piezo-coefficient/ex-030-2SeSn-1_6PbS-1/automaticMethod/calc/piezo/allCoefsTightEdiff-IBRION5-LEPSILON/_src/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+wd=${PWD}
+calcDIR=${wd}/calc/piezo/allCoefsTightEdiff-IBRION5-LEPSILON
+dataDIR=${wd}/data
+bcupDIR=${calcDIR}/_src
+# make any required dirs
+mkdir -p ${calcDIR} ${bcupDIR}
+cp -r ${dataDIR} ${wd}/run.sh ${bcupDIR}
+
+# Copy required files
+cd $dataDIR
+reqFiles="plotPol.py INCAR  KPOINTS  POSCAR  POTCAR runCalcPol*.sbatch"
+cp $reqFiles $calcDIR/
+
+# Some Replacements
+cd $calcDIR
+sbatch runCalcPolShaheen.sbatch
